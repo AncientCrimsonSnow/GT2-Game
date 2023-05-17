802c1ff2ae303fbeeb716d560c302071f18c1b36
Crimson Snow <juri.wiechmann@gmx.net>
2023-05-17 17:19:33 +0200

    Packages

---
+++ Assets/Features/Testing/Testing.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Features.Testing
+{
+    public class Testing : MonoBehaviour
+    {
+        private void Start()
+        {
+            Debug.Log("Hallo World");
+        }
+    }
+}
