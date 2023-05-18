23f558e8073c85af441def99d4b659f6f6aaa56a
Crimson Snow <juri.wiechmann@gmx.net>
2023-05-18 14:17:07 +0200

    MonoTreeExample

---
+++ Assets/Features/TileSystem/MonoTiles/TreeTile.cs
@@ -0,0 +1,13 @@
+﻿using Unity.Mathematics;
+using UnityEngine;
+
+namespace Features.TileSystem.MonoTiles
+{
+    public class TreeTile : MonoBehaviour
+    {
+        private TreeTile tile;
+        
+        //Astetic stuff
+        public int2 MyPos() => tile.MyPos();
+    }
+}
